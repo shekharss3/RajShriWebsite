b97e0d47cf292c1f983dfa1691d2376c52c8cdb3
sshekhawat <sshekhawat@L-NL-20568>
2019-02-15 12:25:09 +0100

    Final website

---
+++ SendEmail.php
@@ -0,0 +1,17 @@
+<?php
+  $posted = false;
+ if(isset($_POST['submit']))
+{  
+    $posted = true;
+    $to = "shekhar.paris@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['name'];
+    $subject = "Form submission";
+    $message = $first_name . " " . " wrote the following:" . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    mail($to,$subject,$message,$headers);
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
